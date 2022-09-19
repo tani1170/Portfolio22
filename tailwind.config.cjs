ccf8ec586cb4d819697740424f1b8f3f73ceb197
Your Name <you@example.com>
2022-09-19 12:27:29 +0200

    Email form test

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{html,vue,js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    colors: {
+      beige_light: "#f8ebdb",
+      beige_dark: "#d4c6a9",
+      green_light: "#a1a67c",
+      green_dark: "#6c7653",
+      dark: "#484F37",
+      white: "#ffffff",
+      black: "#000000",
+    },
+    extend: {
+      backgroundImage: {
+        "footer-figur":
+          "url('img/expand_footer.svg')",
+      },
+    },
   },
-  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+  ],
 };
